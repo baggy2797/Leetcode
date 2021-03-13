17724dc35ec147e9af8e0886753dc3b1ec8e138d
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-03-13 16:58:04 -0500

    Time: 32 ms (63.84%), Space: 14.3 MB (28.58%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        tr = []
+        for i in range(numRows):
+            tr.append([])
+            for j in range(i+1):
+                if j == 0 or i == j:
+                    tr[i].append(1)
+                else:
+                    tr[i].append(tr[i-1][j-1]+tr[i-1][j])
+            # tr.append(temp)
+        return tr
