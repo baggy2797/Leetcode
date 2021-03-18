9a64711567803ab207297cf64df509d63d99a152
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-03-18 00:42:23 -0400

    Time: 32 ms (86.23%), Space: 14.5 MB (89.15%) - LeetHub

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -1,8 +1,25 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        r = ["()"]
-        for _ in range(n-1):
-            r = [i[0:j+1]+"()"+i[j+1:] for i in r for j in range(len(i))]
-            r = list(dict.fromkeys(r))
-        return r
+        res = []
+        def helper(res,curr,left,right):
+            if left<0 or left>right:
+                return
+            if left==0 and right ==0:
+                res.append(curr)
+                return
+            
+            helper(res,curr+"(",left-1,right)
+            helper(res,curr+")",left,right-1)
+        
+        
+        helper(res,"",n,n)
+        return res
+    
+        
+
+        # r = ["()"]
+        # for _ in range(n-1):
+        #     r = [i[0:j+1]+"()"+i[j+1:] for i in r for j in range(len(i))]
+        #     r = list(dict.fromkeys(r))
+        # return r
         
