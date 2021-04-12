06e8abce1de1c391af705581153e88cb5cd46a41
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-04-12 15:05:02 -0400

    Time: 36 ms (56.00%), Space: 14.2 MB (84.53%) - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -1,14 +1,15 @@
-import numpy as np
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()
-        print(matrix)
-        for i in range(len(matrix)):
-            for j in range(i):
-                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-                
-                
         
+        N = len(matrix[0])
+        
+        for i in range(N//2 + N%2):
+            for j in range(N//2):
+                temp = matrix[N-1-j][i]
+                matrix[N-1-j][i] = matrix[N-1-i][N-1-j]
+                matrix[N-1-i][N-1-j] = matrix[j][N-1-i]
+                matrix[j][N-1-i] = matrix[i][j]
+                matrix[i][j] = temp
