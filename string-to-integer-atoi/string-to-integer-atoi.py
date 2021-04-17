10f4d5a92cb4c394937a35de3ed6e941e044b5bb
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-04-17 19:12:47 -0400

    Time: 36 ms (54.10%), Space: 14.2 MB (55.46%) - LeetHub

--- string-to-integer-atoi/string-to-integer-atoi.py
+++ string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,8 +1,8 @@
 import re
 class Solution:
     def myAtoi(self, str: str) -> int:
-        match = re.match(r'^\s*([+-]?\d+)', str)
-        return min(max((int(match.group(1)) if match else 0), -2**31), 2**31 - 1)
+        match = re.match(r'^\s*([\+\-]?\d+)', str)
+        return min( max( (int(match[0]) if match else 0), -2**31), 2**31 - 1 )
         
         
        
