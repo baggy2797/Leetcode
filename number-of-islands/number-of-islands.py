e7bc5521fc88e0f54563b59ea8991b4f13635796
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-04 22:47:50 -0400

    Time: 136 ms (76.96%), Space: 15.3 MB (70.01%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,14 +1,13 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # self.visited = [[False]* len(grid[0]) for _ in range(len(grid))]
+        
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1" :
-                # and self.visited[i][j] == False:
                     count = count + 1
                     self.findIslands(i,j,grid)
-        print(grid)
+        # print(grid)
         return count
         
     def findIslands(self,x,y,grid):
