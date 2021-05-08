a5b356e489007ec6c2966e46ecbc326ae3c07750
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-08 03:30:49 -0400

    Time: 2036 ms (5.05%), Space: 21.2 MB (5.04%) - LeetHub

--- zigzag-conversion/zigzag-conversion.py
+++ zigzag-conversion/zigzag-conversion.py
@@ -1,21 +1,48 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if len(s) == 0 or len(s) == 1 or numRows ==1:
+        length = len(s)
+
+        if numRows == 1:
             return s
+        elif numRows == 2:
+            return s[0:length:2]+s[1:length:2]
+        else:
+            res = [[0 for _ in range(length)] for _ in range(numRows)]
+            rows,cols=len(res),len(res[0])
+
+            r,c = 0,0
+            i = 0
+
+            flag = 0 # 0 meand go down and 1 means go up diagonally 
+            #Go Down Columnwise
+            #column is constant increase the r
+            while i < length:
+                res[r][c] = s[i]
+                if flag == 0:
+                    if r == rows-1:
+                        r-=1
+                        c+=1
+                        flag = 1
+                    else:
+                        r+=1
+                else:
+                    if r == 0:
+                        flag = 0
+                        r+=1
+                    else:
+                        r-=1
+                        c+=1
+                i+=1
+
+            new = ""
+            # print(res)
+            for j in range(numRows):
+                for k in range(length):
+                    if res[j][k] != 0:
+                        new +=(res[j][k])
+            return (new)
+                    
+                    
         
-        
-        rows = [""]*numRows
-        
-        flag = False
-        i = 0
-        for char in s:
-            rows[i] = "".join([rows[i],char])
-            if i == 0 or i == numRows-1:
-                flag = not flag
-            if flag:
-                i=i+1
-            else:
-                i=i-1
-        
-        return "".join(rows)
-               
+            
+            
