556d4d48b3cecf74e38138891f5a1e889fd04f1b
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-21 01:02:59 -0400

    Time: 128 ms (5.51%), Space: 14.6 MB (66.67%) - LeetHub

--- design-log-storage-system/design-log-storage-system.py
+++ design-log-storage-system/design-log-storage-system.py
@@ -2,48 +2,29 @@ class LogSystem:
 
     def __init__(self):
         self.logStorage = defaultdict(list)
-
+    
     def put(self, id: int, timestamp: str) -> None:
-        self.logStorage[timestamp] += [id]
-
+        self.logStorage[timestamp] = id
+        
     def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
+        level = {"Second":6,"Minute":5,"Hour":4,"Day":3,"Month":2,"Year":1}
         res = []
         idx = -1
-        if granularity =="Year":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:1],h2[:1]
-            idx= 0
-        elif granularity =="Month":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:2],h2[:2]
-            idx= 1
-        elif granularity =="Day":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:3],h2[:3]
-            idx= 2
-        elif granularity =="Hour":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:4],h2[:4]
-            idx= 3
-        elif granularity =="Minute":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:5],h2[:5]
-            idx= 4
-        elif granularity =="Second":
-            h1,h2 = start.split(":"),end.split(":")
-            h1,h2 = h1[:6],h2[:6]
-            idx= 5
+        h1,h2 = start.split(":"),end.split(":")
         
-        start_time = int("".join(h1))
-        end_time = int("".join(h2))
+        idx = level[granularity] - 1
+        startTime = int("".join(h1[:idx+1]))
+        endTime = int("".join(h2[:idx+1]))
         
         for key,value in self.logStorage.items():
-            holder = key.split(":")
-            temp = "".join(holder[:idx+1])
-            if int(temp) >= start_time and int(temp)<=end_time:
-                res.append(value[0])
+            temp = "".join(key.split(":")[:idx+1])
+            if startTime <= int(temp) <= endTime:
+                res.append(value)
+        
         return res
             
+        
+    
 
 # Your LogSystem object will be instantiated and called as such:
 # obj = LogSystem()
