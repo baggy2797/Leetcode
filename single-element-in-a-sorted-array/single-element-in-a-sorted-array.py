8d4da74ed4378ef9924e25c5ecc8e193d59ed38e
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-22 01:57:59 -0400

    Time: 72 ms (46.21%), Space: 16.5 MB (33.89%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,22 +1,52 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        length = len(nums)
-        left,right = 0,length-1
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                if nums[mid] == nums[mid - 1]:
+                    left = mid
+                else:
+                    right = mid
+            else:
+                if nums[mid] == nums[mid + 1]:
+                    left = mid
+                else:
+                    right = mid
+        #print(left, right)
+        if left % 2 == 0:
+            return nums[left]
+        return nums[right]
+
+#         length = len(nums)
         
-        while left <= right:
-            mid = (left + right)//2
+#         left,right = 0 ,length-1
+        
+#         while left <= right:
+#             mid = (left+right)//2
             
-            if mid == length -1 or mid == 0:
-                return nums[mid]
+#             if nums[mid] == 0 or nums[mid] == length-1:
+#                 return nums[mid]
+#             elif nums[mid]!= nums[mid-1]!= nums[mid+1]:
+#                 return nums[mid]
             
-            if nums[mid-1] !=nums[mid] and nums[mid]!=nums[mid+1]:
-                return nums[mid]
+#             elif nums[mid]!= nums[mid-1] and mid%2 == 0:
+#                 left = mid
+                
+#             elif nums[mid]!= nums[mid-1] and mid%2 != 0:
+#                 right = mid-1
+                
+#             elif nums[mid]!= nums[mid+1] and mid%2 == 0:
+#                 right = mid
+                
+#             elif nums[mid]!= nums[mid+1] and mid%2 != 0:
+#                 left = mid+1
+                
+#             """
+#             4  5
+#             6  7    mid at 4 mid!=mid+1 and mid%2==0
             
-            elif nums[mid-1]!=nums[mid] and mid%2==0:
-                left = mid+1
-            elif nums[mid-1]!=nums[mid] and mid%2!=0:
-                right = mid
-            elif nums[mid+1]!=nums[mid] and mid%2==0:
-                right = mid
-            elif nums[mid+1]!=nums[mid] and mid%2!=0:
-                left = mid+1
+#             5   6
+#             6   7   mid at 5 and mid!=mid+1 and mid%2!=0
+            
+#             """
