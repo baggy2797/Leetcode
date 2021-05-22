83e45c1d0ad43ea59af869c1a26981a5bfa694ab
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-21 21:46:19 -0400

    Time: 60 ms (13.22%), Space: 15.3 MB (5.93%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        self.cache = {}
+        self.quickCheck = {}
         length = len(nums)
         
-        for idx,num in enumerate(nums):
-            self.cache[num] = idx
+        for i in range(length):
+            self.quickCheck[nums[i]] = i
         
         for i in range(length):
             search = target - nums[i]
-            if search in self.cache and self.cache[search]!=i:
-                return [i,self.cache[search]]
+            if search in self.quickCheck and self.quickCheck[search]!= i:
+                return [i,self.quickCheck[search]]
+        
