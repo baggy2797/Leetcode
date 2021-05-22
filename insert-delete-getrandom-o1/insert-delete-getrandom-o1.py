86f676199f8b7b0c634e6ccad566008f9c6abe3a
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-22 01:38:10 -0400

    Time: 96 ms (72.42%), Space: 18.4 MB (57.66%) - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -4,36 +4,38 @@ class RandomizedSet:
         """
         Initialize your data structure here.
         """
-        self.dict = {}
-        self.list = []
+        self.store = []
+        self.fetching = {}
         
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.dict:
-            return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        if val in self.fetching:return False
+        self.fetching[val] = len(self.store)
+        self.store.append(val)
         return True
 
+    
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.dict:
-            lastElement,idx = self.list[-1],self.dict[val]
-            self.list[idx],self.dict[lastElement] = lastElement,idx
-            self.list.pop()
-            del self.dict[val]
-            return True
-        return False
-            
+        if val not in self.fetching:return False
+        
+        lastElement,val_idx = self.store[-1],self.fetching[val]
+        self.store[val_idx] = lastElement
+        self.fetching[lastElement] = val_idx
+        self.store.pop()
+        del self.fetching[val]
+        return True
+
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return choice(self.list)
+        return random.choice(self.store)
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
