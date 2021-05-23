e395b25337feb5f847e0f5c58f75d92ebd7d8820
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-23 14:38:47 -0400

    Time: 320 ms (35.31%), Space: 22.9 MB (11.83%) - LeetHub

--- palindromic-substrings/palindromic-substrings.py
+++ palindromic-substrings/palindromic-substrings.py
@@ -3,12 +3,12 @@ class Solution:
         dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
         res = 0
         
-        
+        #for characters of length 1: make it 1
         for i in range(len(s)):
             dp[i][i] = 1
             res+=1
             
-            
+        # for cubstrings of length 2 : if both characters are equal : make it 1
         for i in range(len(s)-1):
             if s[i] == s[i+1]:
                 dp[i][i+1] = 1
@@ -16,12 +16,21 @@ class Solution:
         
         # print(dp)
         
-        
+        #for characters of length 3 and above: 
         for length in range(3,len(s)+1):
             for i in range(len(s)-length+1):
-                j = i+ length-1
-                if dp[i+1][j-1] and s[i]==s[j]:
-                    dp[i][j]=1
+                j = i + length-1
+                if dp[i+1][j-1] and s[i] == s[j]:
+                    dp[i][j] = 1
                     res+=1
+        
         return res
+        
+        # for length in range(3,len(s)+1):
+        #     for i in range(len(s)-length+1):
+        #         j = i+ length-1
+        #         if dp[i+1][j-1] and s[i]==s[j]:
+        #             dp[i][j]=1
+        #             res+=1
+        # return res
         
