b9cfed81e3a01f0071bf0e3ade4b9635cb329ebe
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-22 20:12:10 -0400

    Time: 316 ms (67.23%), Space: 19.5 MB (51.86%) - LeetHub

--- minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -1,18 +1,13 @@
 class Solution:
-    def minIncrementForUnique(self, A: List[int]) -> int:
-        A.sort()
-        summOfA = sum(A)
-        length = len(A)
-        for i in range(1,length):
-            if A[i] <= A[i-1]:
-                A[i] = max(A[i],A[i-1]+1)
-        summOfAPost = sum(A)
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
         
-        return summOfAPost - summOfA
-                
+        nextNum = float('-inf')
+        steps = 0
         
+        for num in nums:
+            steps += max(nextNum,num) - num
+            nextNum = max(num,nextNum)+1
+        return steps            
         
         
-        
-        
-            
