545a8bbf3dc1836bc073dd05d008d2d20f36f3f4
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-23 02:55:24 -0400

    Time: 148 ms (26.70%), Space: 24.7 MB (5.00%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.py
+++ merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,26 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if lists == []:
+            return None
+        if lists is None:
+            return lists
+        while len(lists) > 1:
+            l1,l2 = lists.pop(0),lists.pop(0)
+            lists.append(self.mergeTwoLists(l1,l2))
+            
+        return lists[0] if lists else []
         
-        def merger(l1,l2):
-            if l1 is None:
-                return l2
-            elif l2 is None:
-                return l1
-            elif l1.val<= l2.val:
-                l1.next = merger(l1.next,l2)
-                return l1
-            else:
-                l2.next = merger(l1,l2.next)
-                return l2
-        
-        while len(lists)>=2:
-            a1 = lists.pop(0)
-            a2 = lists.pop(0)
-            merged = merger(a1,a2)
-            lists.append(merged)
-        
-        if lists:return lists[0]
-        return None
-        
-    
+    def mergeTwoLists(self,l1,l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
