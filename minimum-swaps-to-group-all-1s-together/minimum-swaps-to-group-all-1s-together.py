bb535f24292a5cdaf42319e71d291262e32f1636
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-05-29 02:49:31 -0400

    Time: 824 ms (44.97%), Space: 17.9 MB (8.10%) - LeetHub

--- minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
+++ minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.py
@@ -1,18 +1,31 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
-        windowSize = sum(data)
-        countOne = maxOne = 0
-        left = right = 0
+        #time complexity : O(2n) = O(n)
+        #space complexity : O(1)
         
-        while right < len(data):
-            countOne+= data[right]
+        if not data:return 0
+        
+        globalMin = 0
+        length = len(data)
+        # count the number of ones
+        CountOfOnes = sum(data) #O(n)
+        #since this is going to be the data frame(windowsize)
+        windowSize = CountOfOnes
+        #check by going on sliding the window through the array till the last element matches the window end and no further
+        left = 0
+        right = 0
+    
+        count = 0
+        while right < length:
+            count += data[right]
             right+=1
-            
             if right - left > windowSize:
-                countOne -= data[left]
+                count-=data[left]
                 left+=1
-            maxOne = max(maxOne,countOne)
+        
+            globalMin = max(globalMin,count)
             
-        return windowSize - maxOne
+        return windowSize - globalMin
+        #keeping a global Minimum to be comapred everytime so as to get the required minimum
                 
         
