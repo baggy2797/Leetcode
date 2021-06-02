5bc1d26d4c54c8aeb487fb21ea3fa488a123792b
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-02 17:02:49 -0400

    Time: 176 ms (34.35%), Space: 18.6 MB (23.65%) - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,41 +1,46 @@
 class RandomizedSet:
-
+    #Every function must operate in O(1) runtime
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.store = []
-        self.fetching = {}
+        self.storage = []
+        self.fetcher = {}
+        self.length = 0
+        
         
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.fetching:return False
-        self.fetching[val] = len(self.store)
-        self.store.append(val)
+        if val in self.fetcher:return False
+        self.storage.append(val)
+        self.fetcher[val] = self.length
+        self.length+=1
         return True
-
     
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val not in self.fetching:return False
+        if val not in self.fetcher:return False
+        #swap the last and the required element
+        lastElement = self.storage[self.length-1]
+        val_idx = self.fetcher[val]
         
-        lastElement,val_idx = self.store[-1],self.fetching[val]
-        self.store[val_idx] = lastElement
-        self.fetching[lastElement] = val_idx
-        self.store.pop()
-        del self.fetching[val]
+        self.storage[val_idx],self.storage[self.length-1] = self.storage[self.length-1],self.storage[val_idx]
+        self.fetcher[lastElement] = val_idx
+        del self.fetcher[self.storage[self.length-1]]
+        self.length-=1
+        self.storage.pop()
         return True
+        
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return random.choice(self.store)
-        
+        return random.choice(self.storage)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
