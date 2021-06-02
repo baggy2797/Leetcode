985478918535a6838033032079c34ae51f8726f3
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-01 20:54:39 -0400

    Time: 5156 ms (25.21%), Space: 14.4 MB (42.02%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,28 +1,19 @@
 class Solution:
+    def dfs(self,board,x,y,word):
+        if len(word) == 0:return True
+        if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or board[x][y]!= word[0]:
+            return False
+        temp = board[x][y] 
+        board[x][y] = '#'
+        ret = self.dfs(board,x+1,y,word[1:]) or self.dfs(board,x,y+1,word[1:]) or self.dfs(board,x-1,y,word[1:]) or self.dfs(board,x,y-1,word[1:])
+        board[x][y] = temp    
+        return ret
+    
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows,cols = len(board),len(board[0])
-        visited = [[False for _ in range(cols)]for _ in range(rows)]
-        
-        def dfs(board,x,y,idx,word):
-            if len(word) == 0:
-                return True
-            if x < 0 or x >= rows or y < 0 or y >= cols or board[x][y]!= word[0]:
-                return False
-            
-            
-            board[x][y] = '#'
-            
-            ret = False
-            
-            ret = dfs(board,x+1,y,idx+1,word[1:]) or dfs(board,x,y+1,idx+1,word[1:]) or dfs(board,x-1,y,idx+1,word[1:]) or dfs(board,x,y-1,idx+1,word[1:])
-            
-            # if ret:break
-            board[x][y] = word[0]
-            return ret
-            
         for row in range(rows):
             for col in range(cols):
-                if dfs(board,row,col,0,word):
-                    return True
-        
+                if self.dfs(board,row,col,word):return True
         return False
+                
+            
