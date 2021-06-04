f4f64ebe9c2b55ca08ca91d28511924d7b743d41
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-03 21:56:10 -0400

    Time: 32 ms (53.03%), Space: 14.1 MB (82.01%) - LeetHub

--- reaching-points/reaching-points.py
+++ reaching-points/reaching-points.py
@@ -1,22 +1,17 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
-       #working backwards
-    
-        #go from tx,ty to sx,sy
         while tx >= sx and ty >= sy:
             if tx == ty:
                 break
-    
             elif tx > ty:
                 if ty > sy:
-                    tx%= ty
+                    tx %= ty
                 else:
                     return (tx-sx) % ty == 0
-                
+            
             else:
                 if tx > sx:
-                    ty%=tx
+                    ty %= tx
                 else:
-                    return (ty-sy)% tx == 0
-                
+                    return (ty-sy) % tx == 0
         return tx == sx and ty == sy
