6b4fd37dfb79dc519dafe2e946b2a840c2d5e5a3
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-03 20:13:29 -0400

    Time: 480 ms (24.42%), Space: 19.4 MB (23.00%) - LeetHub

--- tweet-counts-per-frequency/tweet-counts-per-frequency.py
+++ tweet-counts-per-frequency/tweet-counts-per-frequency.py
@@ -1,41 +1,27 @@
 class TweetCounts:
 
     def __init__(self):
-        self.tweetStorage = {}
-        
+        self.storage = {}
+
     def recordTweet(self, tweetName: str, time: int) -> None:
-        if tweetName not in self.tweetStorage:
-            self.tweetStorage[tweetName] = [time]
-        else:
-            self.tweetStorage[tweetName].append(time)
+        if tweetName not in self.storage:self.storage[tweetName]= [time]
+        else:self.storage[tweetName].append(time)
+        
 
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
-        times = self.tweetStorage[tweetName]
+        freqInSeconds = {"minute":60,"hour":3600,"day":86400}
         
-        size = 0
-        secs = 0
+        times = self.storage[tweetName]
+        secs = freqInSeconds[freq]
+        size = (endTime - startTime) //secs  + 1
+        result = [0] * size
         
-        if freq == "minute":
-            secs = 60
-            size = (endTime-startTime)/ 60 + 1
-        elif freq == "hour":
-            secs = 3600
-            size = (endTime-startTime)/ 3600 + 1
-        else:
-            secs = 86400
-            size = (endTime-startTime)/ 86400 + 1
-        
-        
-        results = [0]*int(size)
         
         for i in times:
             if startTime <= i <= endTime:
                 idx = int((i-startTime)/secs)
-                results[idx] +=1
-            
-        return results
-            
-
+                result[idx]+=1
+        return result
 
 # Your TweetCounts object will be instantiated and called as such:
 # obj = TweetCounts()
