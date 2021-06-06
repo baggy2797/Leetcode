1f96fd484625b158b001d75eb2933a23e6e3a55a
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-06 18:31:06 -0400

    Time: 104 ms (18.99%), Space: 15.1 MB (24.58%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if nums is None:
-            return []
-        self.fetcher = {}
-        length = len(nums)  #O(n)
+        length = len(nums)
         
-        for i in range(length):  #O(n)
-            self.fetcher[nums[i]] = i
-            
-        for i in range(length):    #O(n)
+        fetcher = {}
+        
+        for i in range(length):
             search = target - nums[i]
+            if search in fetcher and fetcher[search]!=i:
+                return [i,fetcher[search]]
+            else:
+                fetcher[nums[i]] = i
             
-            if search in self.fetcher and self.fetcher[search]!=i: #O(1) lookup and checking for equality
-                return [i,self.fetcher[search]]
+            
