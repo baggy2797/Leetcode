4791be741edb177d18d53f7bc99995488bb50672
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-06 18:58:22 -0400

    Time: 332 ms (35.55%), Space: 22.6 MB (24.73%) - LeetHub

--- palindromic-substrings/palindromic-substrings.py
+++ palindromic-substrings/palindromic-substrings.py
@@ -1,38 +1,29 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         '''
-            a a a
-        a   1 3 
-        a     2 
-        a       3
-        '''
-        
-        """
             a b c
-        a   1 1 1
-        b     1 1
-        c       1
-        
-        
-        """
+        a   1 0 0
+        b   0 1 0
+        c   0 0 1
+        '''
         length = len(s)
+        # rows,cols = length,length
         dp = [[0 for _ in range(length)] for _ in range(length)]
-        count = 0
+        ans= 0
         for i in range(length):
-            dp[i][i] =1
-            count+=1
-        
+            dp[i][i] = 1
+            ans+=1
+            
         for i in range(length-1):
+            # print(s[i],s[i+1])
             if s[i] == s[i+1]:
-                dp[i][i+1] = 1
-                count+=1
-        
-        for l in range(3,length+1):
-            for i in range(length-l+1):
-                j = i+l-1
-                if dp[i+1][j-1] and s[i] == s[j]:
-                    dp[i][j] =1
-                    count+=1
-                    
-        return count
-        
+                dp[i][i+1] =1
+                ans+=1
+                
+        for Len in range(3,length+1):
+            for i in range(length-Len+1):
+                j = i + Len-1
+                if s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = 1
+                    ans+=1
+        return ans
