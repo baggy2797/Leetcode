78219ea01702eff411c9357d77832189bbd36979
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-05 20:04:41 -0400

    Time: 132 ms (66.99%), Space: 16.5 MB (83.15%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dicti={}
-        for i in nums:
-            if i not in dicti:
-                dicti[i] = 1
-            else:
-                dicti[i] = dicti[i]+1
-        print(dicti)
-        for i in dicti:
-            if dicti[i] == 1:
-                return i
-            
+        res = nums[0]
+        for i in range(1,len(nums)):
+            res ^= nums[i]
+        return res
