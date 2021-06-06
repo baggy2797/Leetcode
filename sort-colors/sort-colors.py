c3702219b7048ddec3a3adda2fb4655b7b4c996c
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-06 18:06:28 -0400

    Time: 32 ms (74.49%), Space: 14.2 MB (43.42%) - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -4,3 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
+#         length = len(nums)
+#         left = 0
+#         right = length-1
+        
+#         while left <= right:
+#             if nums[left] is not 1 and nums[right] is not 1:
+#                 if nums[left] == 0:
+#                     left+=1
+                
+#                 if nums[right] == 2:
+#                     right-=1
+                 
+#                 elif nums[left] == 2 or nums[right] == 1:
+#                     nums[right],nums[left] = nums[left],nums[right]
+#                     left+=1
+#                     right-=1
