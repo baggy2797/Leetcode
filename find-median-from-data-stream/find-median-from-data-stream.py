84501721a55e1d3468ad0f1b212ef505b910f0ae
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 15:52:04 -0400

    Time: 204 ms (49.23%), Space: 26 MB (5.16%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.py
+++ find-median-from-data-stream/find-median-from-data-stream.py
@@ -4,25 +4,23 @@ class MedianFinder:
         """
         initialize your data structure here.
         """
-        self.minHeap = [] # to store all the greater values -1
-        self.maxHeap = [] # to store all the lesser values  +1
-        # heapq.heapify(self.minHeap)
-        # heapq.heapify(self.maxHeap)
-
-    def addNum(self, num: int) -> None:
-        #add the number first to the maxHeap
-        heappush(self.minHeap,-num)
-        heappush(self.maxHeap,-heappop(self.minHeap))
+        self.maxHeap = [] # store all the smaller values here 
+        #send all the value as negative of themselves (above)
         
-        #if length(maxHeap) > length(minHeap)
-        if len(self.maxHeap) > len(self.minHeap):
+        self.minHeap = [] # store all the larger values here  
+        
+    def addNum(self, num: int) -> None:
+        heappush(self.minHeap,-num) #put the number in the pool of bigger numbers
+        heappush(self.maxHeap,-heappop(self.minHeap)) # pop the smallest number from that pool and push it to the smaller numbers
+        
+        #check if the length of minHeap < maxHeap then append a number from maxHeap to minHeap
+        if len(self.minHeap) < len(self.maxHeap):
             heappush(self.minHeap,-heappop(self.maxHeap))
             
     def findMedian(self) -> float:
-        if len(self.maxHeap) == len(self.minHeap):
-            return (self.maxHeap[0]-self.minHeap[0])/2
-        else:
-            return float(-self.minHeap[0])
+        if len(self.minHeap) > len(self.maxHeap):
+            return -self.minHeap[0]
+        else:return (self.maxHeap[0] - self.minHeap[0]) /2
 
 
 # Your MedianFinder object will be instantiated and called as such:
