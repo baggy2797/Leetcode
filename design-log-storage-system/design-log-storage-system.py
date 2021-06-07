c4039d6e6e01cd06498be033f267f1b27546e345
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 16:54:39 -0400

    Time: 52 ms (73.48%), Space: 14.7 MB (36.74%) - LeetHub

--- design-log-storage-system/design-log-storage-system.py
+++ design-log-storage-system/design-log-storage-system.py
@@ -1,30 +1,23 @@
 class LogSystem:
 
     def __init__(self):
-        self.logStorage = defaultdict(list)
-    
+        self.LogStorage = {}
+        
     def put(self, id: int, timestamp: str) -> None:
-        self.logStorage[timestamp] = id
-        
+        self.LogStorage[tuple(timestamp.split(':'))] =id
+
     def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
-        level = {"Second":6,"Minute":5,"Hour":4,"Day":3,"Month":2,"Year":1}
-        res = []
-        idx = -1
-        h1,h2 = start.split(":"),end.split(":")
+        getGranularity = {"Year":1,"Month":2,"Day":3,"Hour":4,"Minute":5,"Second":6}
+        index = (getGranularity[granularity])
+        start = tuple(start.split(":")[:index])
+        end = tuple(end.split(":")[:index])
+        ans = []
         
-        idx = level[granularity] - 1
-        startTime = int("".join(h1[:idx+1]))
-        endTime = int("".join(h2[:idx+1]))
+        for t in self.LogStorage.keys():
+            if start <= t[:index] <= end:
+                ans.append(self.LogStorage[t])
+        return ans
         
-        for key,value in self.logStorage.items():
-            temp = "".join(key.split(":")[:idx+1])
-            if startTime <= int(temp) <= endTime:
-                res.append(value)
-        
-        return res
-            
-        
-    
 
 # Your LogSystem object will be instantiated and called as such:
 # obj = LogSystem()
