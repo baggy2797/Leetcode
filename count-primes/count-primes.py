8dba04694029d0ae1a3b0bfb8d89cd22574e80b6
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 00:26:04 -0400

    Time: 744 ms (87.23%), Space: 92.1 MB (33.22%) - LeetHub

---
+++ count-primes/count-primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        s = [1] * n
+        s[0] = s[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if s[i] == 1:
+                s[i * i:n:i] = [0] * len(s[i * i:n:i])
+        return sum(s)
+        return False
+                
