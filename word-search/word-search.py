3af9757d76cf7c6f7d0c50b0de460b4a8d795450
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 15:33:48 -0400

    Time: 5152 ms (29.87%), Space: 14.3 MB (70.16%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,19 +1,23 @@
 class Solution:
-    def dfs(self,board,x,y,word):
-        if len(word) == 0:return True
-        if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or board[x][y]!= word[0]:
-            return False
-        temp = board[x][y] 
-        board[x][y] = '#'
-        ret = self.dfs(board,x+1,y,word[1:]) or self.dfs(board,x,y+1,word[1:]) or self.dfs(board,x-1,y,word[1:]) or self.dfs(board,x,y-1,word[1:])
-        board[x][y] = temp    
-        return ret
-    
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows,cols = len(board),len(board[0])
         for row in range(rows):
             for col in range(cols):
-                if self.dfs(board,row,col,word):return True
+                if board[row][col] == word[0]:
+                    if self.dfs(board,row,col,word):
+                        return True
         return False
-                
-            
+    
+    
+    def dfs(self,board,x,y,word):
+        if len(word) == 0:
+            return True
+        if x < 0 or x >= len( board) or y < 0 or y >= len(board[0]) or board[x][y]!=word[0]:
+            return False
+        
+        temp = board[x][y]
+        board[x][y] = '#'
+        
+        ret = self.dfs(board,x+1,y,word[1:]) or self.dfs(board,x,y+1,word[1:]) or self.dfs(board,x-1,y,word[1:]) or self.dfs(board,x,y-1,word[1:])
+        board[x][y] = temp
+        return ret
