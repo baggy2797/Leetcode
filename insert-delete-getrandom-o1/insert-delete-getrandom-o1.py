c983d9101b29941055c1b73f453018837bd991e0
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-06 21:33:26 -0400

    Time: 108 ms (44.88%), Space: 18.5 MB (57.82%) - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,46 +1,48 @@
 class RandomizedSet:
-    #Every function must operate in O(1) runtime
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.storage = []
-        self.fetcher = {}
+        self.array = []
+        self.hashMap = {}
         self.length = 0
-        
         
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val in self.fetcher:return False
-        self.storage.append(val)
-        self.fetcher[val] = self.length
+        if val in self.hashMap:return False
+        self.array.append(val)
+        self.hashMap[val] = self.length
         self.length+=1
         return True
-    
+        
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val not in self.fetcher:return False
-        #swap the last and the required element
-        lastElement = self.storage[self.length-1]
-        val_idx = self.fetcher[val]
-        
-        self.storage[val_idx],self.storage[self.length-1] = self.storage[self.length-1],self.storage[val_idx]
-        self.fetcher[lastElement] = val_idx
-        del self.fetcher[self.storage[self.length-1]]
+        if val not in self.hashMap:return False
+        if val == self.array[-1]:
+            del self.hashMap[val]
+            self.array.pop()
+        else:
+            lastElement,lastElementIdx = self.array[-1],self.length-1
+            val_idx = self.hashMap[val]
+            
+            # print(self.array,self.hashMap)
+            self.array[val_idx],self.array[lastElementIdx] = self.array[lastElementIdx],self.array[val_idx]
+            self.hashMap[lastElement] = val_idx
+            self.array.pop()
+            del self.hashMap[val]
         self.length-=1
-        self.storage.pop()
         return True
-        
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return random.choice(self.storage)
+        return random.choice(self.array)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
