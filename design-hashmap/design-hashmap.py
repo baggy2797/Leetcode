ba8953dd1412cf02e5b037507b125b5d5f5b13a5
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 00:44:25 -0400

    Time: 436 ms (26.15%), Space: 17.8 MB (34.40%) - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,70 +1,63 @@
 class Bucket:
     def __init__(self):
         self.bucket = []
-
-    def get(self, key):
-        for (k, v) in self.bucket:
+        
+    def getVal(self,key):
+        for (k,v) in (self.bucket):
             if k == key:
                 return v
         return -1
-
-    def update(self, key, value):
+        
+    def update(self,key,value):
         found = False
-        for i, kv in enumerate(self.bucket):
-            if key == kv[0]:
-                self.bucket[i] = (key, value)
+        for i,kv in enumerate(self.bucket):
+            if kv[0] == key:
                 found = True
+                self.bucket[i] = (key,value)
                 break
-
         if not found:
-            self.bucket.append((key, value))
-
-    def remove(self, key):
-        for i, kv in enumerate(self.bucket):
+            self.bucket.append((key,value))
+            
+    def remove(self,key):
+        for i,kv in enumerate(self.bucket):
             if key == kv[0]:
                 del self.bucket[i]
-
-
-class MyHashMap(object):
+            
+class MyHashMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        # better to be a prime number, less collision
-        self.key_space = 2069
-        self.hash_table = [Bucket() for i in range(self.key_space)]
+        self.hashValue = 2069
+        self.Map = [Bucket() for i in range(self.hashValue)]
 
 
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
-        :type key: int
-        :type value: int
-        :rtype: None
         """
-        hash_key = key % self.key_space
-        self.hash_table[hash_key].update(key, value)
+        hashKey = key % self.hashValue
+        self.Map[hashKey].update(key,value)
+        
 
-
-    def get(self, key):
+    def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
         """
-        hash_key = key % self.key_space
-        return self.hash_table[hash_key].get(key)
+        
+        hashKey = key % self.hashValue
+        # print(self.Map[hashKey].bucket)
+        return self.Map[hashKey].getVal(key)
 
-
-    def remove(self, key):
+    def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: None
         """
-        hash_key = key % self.key_space
-        self.hash_table[hash_key].remove(key)
+        hashKey = key % self.hashValue
+        self.Map[hashKey].remove(key)
+        
+
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
