adf8f9f9ac1ab7c7b51f1d807d34b7111dc43f29
Aditya Nitin Bhagwat <bhagwataditya226@gmail.com>
2021-06-07 01:22:28 -0400

    Time: 88 ms (45.58%), Space: 14.5 MB (28.23%) - LeetHub

--- longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
@@ -1,41 +1,21 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        if not s:return 0
-        if k>=len(s):return len(s)
-        if k == 0:return 0
+        frequency = {}
+        left =0 
+        longest = 0
         
-        
-        length = len(s)
-        
-        windowSize = 0
-        right = 0
-        left = 0
-        temp = {}
-        maxLength = 1
-        
-        while right < length: 
-            if s[right] in temp:
-                temp[s[right]]+=1
-            else:
-                windowSize+=1
-                temp[s[right]] = 1
+        for right,char in enumerate(s):
+            frequency[char] =frequency.get(char,0)+1
             
-            if windowSize<= k:
-                maxLength = max(maxLength,right - left + 1)
-            
-            else:
-                if s[left] in temp:
-                    temp[s[left]] -= 1
-                    if temp[s[left]] == 0:
-                        del temp[s[left]]
-                        windowSize-=1
+            while len(frequency) > k:
+                # temp = s[left]
+                frequency[s[left]]-=1
+                if frequency[s[left]] == 0:
+                    del frequency[s[left]]
                 left+=1
             
-            right+=1
-        
-            # print(left,right)
-            # print(temp)
-        return (maxLength)
-                    
-        
+            size = right-left+1
+            longest = max(longest,size)
+            
+        return longest
         
